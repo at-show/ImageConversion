ad998a0843112dff9809d524538bd0ec37751c06
at-show <atshow@gmail.com>
2016-07-09 00:03:43 +0900

    Revert "出力ログファイル名を変えた"
    This reverts commit 2909c02709e874434c820d520d8051daa958b586.

--- 00_ConversionStart.bat
+++ 00_ConversionStart.bat
@@ -1,3 +1,3 @@
-echo ########## start ########## [%date% %time%] >> 02_Conversion.log
-cscript 01_Conversion.vbs >> 02_Conversion.log 2>&1
-echo ##########  end  ########## [%date% %time%] >> 02_Conversion.log
+echo ########## start ########## [%date% %time%] >> 03_Conversion.log
+cscript 01_Conversion.vbs >> 03_Conversion.log 2>&1
+echo ##########  end  ########## [%date% %time%] >> 03_Conversion.log
